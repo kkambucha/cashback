d4d4fa629c81fc1c62d72a642c39f3cc065a01a8
Vadim Zakovryashin <vadim.zakovryashin@starcode.ru>
2015-10-28 11:33:03 +0300

    Стилизация селекта

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 /*
  * Third party
  */
-/* //= ../../bower_components/jquery/dist/jquery.js */
-
+//= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/select2/dist/js/select2.js
+//= ../../bower_components/bxslider-4/dist/jquery.bxslider.js
 
 /*
  * Custom
  */
+ 
 //= partials/app.js
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,7 +1,13 @@
 (function(){
+	
 	if($('#b-slider').length > 0){
 		$('#b-slider').bxSlider({
 			'controls' : false
 		});
 	}   
+
+	if($('#b-region').length > 0){
+		$('#b-region').select2({theme: "classic"});	
+	}
+	
 })();
