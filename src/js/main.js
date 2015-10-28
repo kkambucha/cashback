f84463b968aa7dda8252bafb7391c716eb7d1ed3
Vadim Zakovryashin <vadim.zakovryashin@starcode.ru>
2015-10-28 14:51:00 +0300

    Еще несколько страниц

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
  */
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/select2/dist/js/select2.js
-//= ../../bower_components/bxslider-4/dist/jquery.bxslider.js
+//= ../../bower_components/bxslider-4/jquery.bxslider.js
 
 /*
  * Custom
