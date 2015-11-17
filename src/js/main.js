7b1167fbae2f59f5a423a9694cdafaa4af23b8bc
Vadim Zakovryashin <vadim.zakovryashin@starcode.ru>
2015-11-17 17:46:13 +0300

    Добавил страницу Сертификата crt-print.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,13 @@
 /*
  * Third party
  */
+
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/select2/dist/js/select2.js
 //= ../../bower_components/bxslider-4/jquery.bxslider.js
+//= ../../bower_components/jquery-validation/dist/jquery.validate.min.js
 //= ../../bower_components/jquery-ui/jquery-ui.min.js
+//= ../../bower_components/bPopup/jquery.bpopup.min.js
 
 /*
  * Custom
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -18,5 +18,46 @@
 			active : 1
 		});
 	}
+
+	// валидация форм
+	// задаем default-текст сообщений
+	$.extend($.validator.messages, {
+	  required: 'Поле обязательно для заполнения',
+	  email: "Пожалуйста, введите корректный email"
+	});
+	// задаем правильную разметку вывода сообщений
+	$.validator.setDefaults({
+	  	errorPlacement: function(error, element, label) {
+			var container = $('<div class="b-login__form__item error"><span></span></div>');
+			container.insertAfter(element.closest('.b-login__form__item'));
+			error.appendTo(container);
+		}
+	});
+
+	$.validator.addClassRules({
+	  name: {
+	    required: true,
+	    minlength: 2
+	  },
+	  email: {
+	    required: true,
+	    email: true
+	  }
+	});
+
+	// назначаем валидацию на нужные формы
+	if($('.c-validate').length > 0){
+		$('.c-validate').each(function() {
+			$(this).validate();
+		});
+	}
+
+	// открываем popup
+	$(document).ready(function(){
+
+		$('#element_to_pop_up').bPopup();	
+
+	});
 	
+
 })();
